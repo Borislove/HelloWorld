1db1fb5742b2aa57a106d1618ab5ae64664afb2f
Borislove <abirme@yandex.ru>
2020-05-31 11:45:32 +0300

    тест

---
+++ com/javacourse/se/lesons7/Bus.java
@@ -0,0 +1,11 @@
+package com.javacourse.se.lesons7;
+
+//Java SE. Урок 7. Введение в классы и объекты
+public class Bus {
+    public String model;
+    public String color;
+
+    public void showColor(){
+        System.out.println(color);
+    }
+}
