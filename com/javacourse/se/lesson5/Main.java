6aae1c025ed4c1f87d16a511adfa2f3461ca5fc0
Borislove <abirme@yandex.ru>
2020-05-31 11:50:47 +0300

    rename package XX

---
+++ com/javacourse/se/lesson5/Main.java
@@ -0,0 +1,40 @@
+package com.javacourse.se.lesson5;
+
+import com.javacourse.se.lesson7.Bus;
+
+public class Main {
+
+    /////////////////////////////////////////////////////
+    //1 способ
+    /*public static void main(String[] args) {
+    }*/
+
+   /* //2 способ
+    public static void main(String... args) {
+        System.out.println("Hot Java");
+    }*/
+
+    //3 способ
+    /*public static void main(String args[]) {
+    }*/
+
+    ////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+
+        Bus bus ;
+        bus = new Bus();
+
+        Bus ourBus = new Bus();
+        Bus firstBus = new Bus();
+        Bus secondBus =new Bus();
+
+        ourBus.color = "Green";
+        firstBus.color = "Red";
+        secondBus.color = "Gray";
+
+        ourBus.showColor();
+        firstBus.showColor();
+        secondBus.showColor();
+    }
+}
---
+++ com/javacourse/se/lesson9/Operation.java
@@ -0,0 +1,4 @@
+package com.javacourse.se.lesson9;
+
+public class Operation {
+}
