47429d0030e95dcbb2f34bfbba8135636d9ce561
Borislove <abirme@yandex.ru>
2020-05-31 10:24:04 +0300

    Java SE. Урок 5. Структура проекта, понятие пакета, правила именования . Первый проект на java (String... args) <---

---
+++ com/javacourse/se/lesson5/Main.java
@@ -0,0 +1,17 @@
+package com.javacourse.se.lesson5;
+
+public class Main {
+
+    //1 способ
+    /*public static void main(String[] args) {
+    }*/
+
+    //2 способ
+    public static void main(String... args) {
+        System.out.println("Hot Java");
+    }
+
+    //3 способ
+    /*public static void main(String args[]) {
+    }*/
+}
