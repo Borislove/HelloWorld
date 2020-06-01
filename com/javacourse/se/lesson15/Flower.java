41cfffec826cc85d73f9e3ed8ce9ab4b5d57d80b
Borislove <abirme@yandex.ru>
2020-06-01 08:01:00 +0300

    Java SE. Урок 15. Конструкторы

---
+++ com/javacourse/se/lesson15/Flower.java
@@ -0,0 +1,15 @@
+package com.javacourse.se.lesson15;
+
+public class Flower {
+
+    String name;
+    String color;
+
+    public Flower() {
+    }
+
+    public Flower(String myName, String myColor) {
+        name = myName;
+        color = myColor;
+    }
+}
