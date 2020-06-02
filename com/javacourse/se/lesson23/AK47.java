e144b9a15e0f5ab48808101a3f10db752cc88828
Borislove <abirme@yandex.ru>
2020-06-02 09:01:24 +0300

    Java SE. Урок 24. Интерфейсы

--- com/javacourse/se/lesson23/AK47.java
+++ com/javacourse/se/lesson23/AK47.java
@@ -1,4 +1,9 @@
 package com.javacourse.se.lesson23;
 
 public class AK47 extends Weapon {
+
+    @Override
+    public void foo() {
+
+    }
 }
---
+++ com/javacourse/se/lesson24/Main.java
@@ -0,0 +1,13 @@
+package com.javacourse.se.lesson24;
+
+//Java SE. Урок 24. Интерфейсы
+public class Main {
+    public static void main(String[] args) {
+
+        Mi8 mi8 = new Mi8();
+        Boeing737 boeing737 = new Boeing737();
+
+        Test test = new Test();
+        test.foo(mi8);
+    }
+}
